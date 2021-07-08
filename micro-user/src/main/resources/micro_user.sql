6b57b1f25a537b8f6444ab5fd4fa0f80068dde60
GrootSwt <396518038@qq.com>
2021-07-08 16:55:03 +0800

    更新数据库表结构和数据

--- micro-user/src/main/resources/micro_user.sql
+++ micro-user/src/main/resources/micro_user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 06/07/2021 15:01:11
+ Date: 08/07/2021 16:54:10
 */
 
 SET NAMES utf8mb4;
@@ -28,60 +28,54 @@ CREATE TABLE `menu`  (
   `icon` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '图标',
   `sort` int(11) NOT NULL COMMENT '排序',
   `enabled` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '是否启用（0：未启用，1：启用）',
-  `p_id` bigint(20) NOT NULL COMMENT '父菜单ID',
+  `parent_id` bigint(20) NOT NULL COMMENT '父菜单ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
 INSERT INTO `menu` VALUES (1, '主页', '/home', 'el-icon-s-home', 1, '1', 0);
-INSERT INTO `menu` VALUES (2, '统计分析', '/analysis', 'el-icon-s-marketing', 2, '1', 0);
-INSERT INTO `menu` VALUES (3, '活跃分析', '/analysis/living', 'el-icon-place', 3, '1', 2);
-INSERT INTO `menu` VALUES (4, '时段分析', '/analysis/time', 'el-icon-bicycle', 4, '1', 2);
-INSERT INTO `menu` VALUES (5, '菜单管理', '/menu', 'el-icon-s-tools', 5, '1', 0);
-INSERT INTO `menu` VALUES (6, '人员管理', '/menu/person', 'el-icon-user', 6, '1', 5);
-INSERT INTO `menu` VALUES (7, '角色管理', '/menu/role', 'el-icon-key', 7, '1', 5);
-INSERT INTO `menu` VALUES (8, '权限管理', '/menu/power', 'el-icon-lock', 8, '1', 5);
+INSERT INTO `menu` VALUES (2, '系统管理', '/system', 'el-icon-s-tools', 2, '1', 0);
+INSERT INTO `menu` VALUES (3, '菜单管理', '/system/menu', 'el-icon-menu', 1, '1', 2);
+INSERT INTO `menu` VALUES (11, '角色管理', '/system/role', 'fa fa-chain', 2, '1', 2);
 
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
-  `role_relation_menu_id` bigint(20) NOT NULL COMMENT '角色关联菜单ID',
+  `description` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (1, '管理员', 1);
+INSERT INTO `role` VALUES (1, '普通用户', '没有系统管理权限');
+INSERT INTO `role` VALUES (5, '系统管理用户', '负责系统管理功能');
 
 -- ----------------------------
 -- Table structure for role_relation_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `role_relation_menu`;
 CREATE TABLE `role_relation_menu`  (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `role_id` bigint(20) NOT NULL COMMENT '角色ID',
   `menu_id` bigint(20) NOT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role_relation_menu
 -- ----------------------------
-INSERT INTO `role_relation_menu` VALUES (1, 1, 1);
-INSERT INTO `role_relation_menu` VALUES (2, 1, 2);
-INSERT INTO `role_relation_menu` VALUES (3, 1, 3);
-INSERT INTO `role_relation_menu` VALUES (4, 1, 4);
-INSERT INTO `role_relation_menu` VALUES (5, 1, 5);
-INSERT INTO `role_relation_menu` VALUES (6, 1, 6);
-INSERT INTO `role_relation_menu` VALUES (7, 1, 7);
-INSERT INTO `role_relation_menu` VALUES (8, 1, 8);
+INSERT INTO `role_relation_menu` VALUES (6, 1, 1);
+INSERT INTO `role_relation_menu` VALUES (7, 3, 1);
+INSERT INTO `role_relation_menu` VALUES (8, 3, 2);
+INSERT INTO `role_relation_menu` VALUES (9, 3, 3);
+INSERT INTO `role_relation_menu` VALUES (10, 3, 11);
 
 -- ----------------------------
 -- Table structure for user
@@ -96,11 +90,12 @@ CREATE TABLE `user`  (
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
   `role_id` bigint(20) NOT NULL COMMENT '角色ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '123456', 'admin', '18888888888', '18888888888@micro.com', 1);
+INSERT INTO `user` VALUES (1, 'admin', '123456', 'admin', '18888888888', '18888888888@micro.com', 0);
+INSERT INTO `user` VALUES (2, 'swt', '123456', 'swt', '17777777777', '17777777777@micro.com', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
